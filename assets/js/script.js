e00e4f76f3420c303159199eeb6779cde2c6acba
Remigius Richards <profremy@yahoo.co.uk>
2021-09-14 13:31:46 +0100

    Added mixin to modify cta buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 console.log('script is started');
+
+const elements = {
+  doc: document.getElementsByTagName('body'), // An array of NodeList (not elements)
+  doc: document.querySelector('body'), // Select body tag
+};
+
+{
+  // CONTENT PROTECTION
+  // Right click or context menu prevention
+  // Event listener works only on elements and not a node list hence the iteration to target an element in the body
+  //   You could also simply use querySelector since there is only one body tag so there will not be need to iterate
+  /*
+
+  for (const cur of Array.from(elements.doc)) {
+    cur.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+  }
+
+  */
+
+  elements.doc.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+  });
+
+  elements.doc.addEventListener('copy', (e) => {
+    e.preventDefault();
+  });
+
+  elements.doc.addEventListener('paste', (e) => {
+    e.preventDefault();
+  });
+}
